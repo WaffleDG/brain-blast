069a6ad3454476aa8bcae0130fafd026823d5300
Gregory <51427334+WaffleDG@users.noreply.github.com>
2025-11-17 10:41:29 -0800

    Updated HomePanel and added MainFrame

---
+++ Graphics/MainFrame.java
@@ -0,0 +1,11 @@
+/**
+ *
+ */
+public class MainFrame extends JFrame {
+    public MainFrame() {
+        setTitle("My App");
+        setSize(800, 600);
+        setDefaultCloseOperation(EXIT_ON_CLOSE);
+        setVisible(true);
+    }
+}
