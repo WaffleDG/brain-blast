fdd55df307d915a92596971a9aef3c79b6fc4dfd
Gregory <51427334+WaffleDG@users.noreply.github.com>
2025-11-17 10:07:10 -0800

    Updated to Panel.
    Panels are the individual screens, Frames are the windows. There will be one Frame, with multiple panels.

---
+++ Graphics/HomePanel.java
@@ -0,0 +1,18 @@
+import javax.swing.JPanel;
+
+/**
+ * Defines the Home panel for Brain Blast! This will feature buttons to allow the user to change frames to the
+ * "Set creation" screen and the "see other created sets" screen. This is an implementation of a JPanel. It will be loaded
+ * in the JFrame that defines the window.
+ * 
+ * @author     Gregory Cohen and Riya Jonalla
+ * @since      11/16/2025
+ * @version    1.0
+ * @see        JPanel
+ */
+public class HomePanel extends JPanel {
+   public HomePanel() {
+      return;
+   }
+   
+}
