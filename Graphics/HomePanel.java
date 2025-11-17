dfb15b8ca0002d03a40250d67fcfc7038194e46e
Gregory <51427334+WaffleDG@users.noreply.github.com>
2025-11-17 10:41:29 -0800

    Updated HomePanel and added MainFrame

--- Graphics/HomePanel.java
+++ Graphics/HomePanel.java
@@ -2,8 +2,15 @@ import javax.swing.JPanel;
 
 /**
  * Defines the Home panel for Brain Blast! This will feature buttons to allow the user to change frames to the
- * "Set creation" screen and the "see other created sets" screen. This is an implementation of a JPanel. It will be loaded
- * in the JFrame that defines the window.
+ * "Set creation" screen and the "see other created sets" screen. This is an implementation of a JPanel.
+ * It will be loaded in the JFrame that defines the window.
+ *
+ * <p>
+ * Authors: Gregory Cohen and Riya Jonnala.
+ *
+ * <p>
+ * Version: 1.0, 11/17/2025.
+ *
  * 
  * @author     Gregory Cohen and Riya Jonalla
  * @since      11/16/2025
@@ -11,8 +18,11 @@ import javax.swing.JPanel;
  * @see        JPanel
  */
 public class HomePanel extends JPanel {
+   /**
+    * The HomePanel will be the exact same every time, featuring a background, screen, and buttons as described above.
+    */
    public HomePanel() {
-      return;
+      // calls to this will add to the panel. 
    }
    
 }
