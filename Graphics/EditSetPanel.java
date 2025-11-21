e00a0b08426b2b485b50b7764a5188b7ac243e64
WaffleDG <c_gr_cohen@icloud.com>
2025-11-21 08:48:07 -0800

    Created and started on the EditSetPanel

---
+++ Graphics/EditSetPanel.java
@@ -0,0 +1,45 @@
+import java.util.ArrayList;
+import javax.swing.JPanel;
+import javax.swing.JLabel;
+import javax.swing.JButton;
+import javax.swing.Box;
+import javax.swing.BoxLayout;
+import javax.swing.ImageIcon;
+import javax.imageio.ImageIO;
+import java.awt.Component;
+import java.awt.Dimension;
+import java.awt.Graphics;
+import java.awt.Image;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+
+/**
+ * Screen for creating or editing new sets for Brain Blast! This will provide the user the ability to create a new set, entering as many keyword-definition pairs as they want!
+ * K-D pairs will not be stored in a map; and instead will be stored in two lists for their ability to be shuffled. Again, this is an implementation of a JPanel.
+ * 
+ * <p>
+ * Authors: Gregory Cohen and Riya Jonnala.
+ *
+ * <p>
+ * Version: 1.0, 11/21/2025.
+ *
+ * 
+ * @author     Gregory Cohen and Riya Jonalla
+ * @since      11/21/2025
+ * @version    1.0
+ * @see        JPanel
+ */
+public class EditSetPanel extends JPanel {
+   /**
+    * Constructor which takes a file path, reads the file or creates one if not already existing, loads the set and the graphics from that.
+    */
+   public EditSetPanel(String filePath) {
+      
+   }
+  
+  
+  
+  
+  private static 
+}  
