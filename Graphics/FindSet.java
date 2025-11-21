6bd28709197f8a463a7f3afda8a93ff833eb4050
Jonnala <JonnRiy26@issaquah.wednet.edu>
2025-11-21 08:47:02 -0800

    FindSet class started and comments on what it should do

---
+++ Graphics/FindSet.java
@@ -0,0 +1,35 @@
+public class FindSet {
+
+/**
+ * The frame which is going to be viewed as the app BrainBlast. This is a JPanel and is the screen which is going 
+ * to be viewed. It has a private JPanel which can be accessed from the HomePanel and allows users to view existing saved flashcard sets. 
+ * 
+ *
+ * <p>
+ * Authors: Gregory Cohen and Riya Jonnala.
+ * 
+ * <p>
+ * Version: 1.0, 11/21/2025.
+ *
+ * @author     Gregory Cohen and Riya Jonnala
+ * @since      11/21/2025
+ * @version    1.0
+ * @see        JFrame
+ */
+
+//button to click to access an existing set
+//search by name
+
+   //scanner input to take name of the set (needs to be exact in terms of spacing, caps doesn't matter)
+   //check against existing list of set names (in folder) 
+   //
+//find it on the page (all existing sets pop up)
+//click on a set (each is a button)
+//when clicked, open to see options of what you can do with the set (can edit, quiz, review)
+ 
+
+
+
+
+
+}
