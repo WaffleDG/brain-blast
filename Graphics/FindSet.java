026f069c891ed3461b4993ac9587cc9ae5e385ed
riyajonnala <riya.jonnala@gmail.com>
2025-12-01 08:43:38 -0800

    added FindSet constructor to make a scrollable panel, draw out what the FindSet panel should look like

--- Graphics/FindSet.java
+++ Graphics/FindSet.java
@@ -1,4 +1,5 @@
 import javax.swing.*;
+import java.awt.*;
 import java.io.*;
 import java.util.*;
 
@@ -20,34 +21,62 @@ import java.util.*;
  * @see        JFrame
  */
 
-
 public class FindSet extends JFrame {
-//make it a JScrollPanel
 
 
+    public FindSet() { //FindSet constructor - makes FindSet a JScrollPane
+        setTitle("Find Set Page");
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setSize(800, 600);
+        setLocationRelativeTo(null); //where should the location be??
 
-// find set page
-/*               find set  
+        // Create JPanel
+        JPanel panelOfSets = new JPanel();
+        panelOfSets.setLayout(new GridLayout(100, 5)); // Example: a panel with many rows
+        for (int i = 0; i < 100; i++) { //need to make sure that the buttons only come up based on how many sets exist
+            panelOfSets.add(new JButton("Button " + (i + 1)));
+        }
+
+        // Creates a JScrollPane by passing in the JPanel as the view
+        JScrollPane scrollPane = new JScrollPane(panelOfSets);
+
+        // Customize scroll bar policies (defaults are AS_NEEDED)
+        scrollPane.setVerticalScrollBarPolicy(JScrollPane.VERTICAL_SCROLLBAR_ALWAYS); //can always scroll up and down
+        scrollPane.setHorizontalScrollBarPolicy(JScrollPane.HORIZONTAL_SCROLLBAR_AS_NEEDED); //if needed, scroll right and left
+
+        // Adds the JScrollPane to your frame
+        add(scrollPane, BorderLayout.CENTER);
+
+        setVisible(true);
+    }
+
+
+/*               find set
                search by name
      ________________________________
-     |                               | //has all existing sets in it, can scroll to click one
-     |                               |      
-     |                               |
-     |                               |
-     |                               |
-     |                               |
-     |                               |
-     |                               |
-     |                               |
-     |                               |
-     |                               |
-     |_______________________________|
+     | ____ ____ ____ ____ ____  || | //has all existing sets in it, can scroll to click one (5 in a row, 100 in a column)
+     | |__| |__| |__| |__| |___| || |
+     | ____ ____ ____ ____ ____  || |
+     | |__| |__| |__| |__| |__|  || |
+     | ____ ____ ____ ____ ____  || |
+     | |__| |__| |__| |__| |__|  || |
+     | ____ ____ ____ ____ ____  || |
+     | |__| |__| |__| |__| |__|  || |
+     | ____ ____ ____ ____ ____  || |
+     | |__| |__| |__| |__| |__|  || |
+     |______________________________|
      
 */
 
-
+//find it on the page (all existing sets pop up in the grid with the title in the grid box
+// (potential cover pic for each grid box later))
+    //click on a set (each is a button)
+    //open to see options of what you can do with the set (can edit, quiz (given key or given definition), review)
+    //each is a button which then leads to a new panel
 
 //button to click to access an existing set
+
+    // OR
 
 //search by name
    
@@ -73,24 +102,12 @@ public class FindSet extends JFrame {
        //while (sc.hasNext()) {
 
 
-      for (int i = 0; i < existingSets.size(); i++) { // test comment
-         if (searchedName.toUppercase().equals(name of list of sets(i)) {
+      for (int i = 0; i < existingSets.size(); i++) {
+         if (searchedName.toUppercase().equals(//name of list of sets(i)) {
             return true;
          }
       }
    }
-
-// OR
-
-
-//find it on the page (all existing sets pop up)
-       //click on a set (each is a button)
-       //when clicked, link to gregory's edit set page  and
-       //open to see options of what you can do with the set (can edit, quiz, review)
-       //each is a button which then leads to a new panel
-
-
-
 
 
 
